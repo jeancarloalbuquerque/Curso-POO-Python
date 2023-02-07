037336066d0d4ebcfd40cdbb1b67cb0208cec8d2
Jean Carlo Albuquerque <jeancarlo1584@gmail.com>
2023-02-07 01:36:20 -0400

    Aula 01

---
+++ main.py
@@ -0,0 +1,10 @@
+from pessoa import pessoa
+
+p1 = pessoa("Luiz", 16)
+p2 = pessoa("João", 31)
+
+p1.comer("Banana")
+p1.falar("Buracos Negros")
+p2.falar("Star Wars")
+p2.ano_nascimento()
+p1.ano_nascimento()
---
+++ pessoa.py
@@ -0,0 +1,56 @@
+from datetime import datetime
+
+
+class pessoa:
+    ano_atual = int(datetime.strftime(datetime.now(), "%Y"))
+
+    def __init__(self, nome, idade, comendo=False, falando=False):
+        self.nome = nome
+        self.idade = idade
+        self.comendo = comendo
+        self.falando = falando
+        
+    def comer(self, alimento):
+        if self.comendo:
+            print(f"{self.nome} já está comendo.")
+            return
+        
+        if self.falando:
+            print(f"{self.nome} está falando, não pode comer.")
+        
+        self.comendo = True
+        print(f"{self.nome} está comendo {alimento}.")
+    
+    def parar_comer(self):
+        if not self.comendo:
+            print(f"{self.nome} não está comendo.")
+            return
+        
+        self.comendo = False
+        print(f"{self.nome} parou de comer.")
+        
+    def falar(self, assunto):
+        if self.falando:
+            print(f"{self.nome} já está falando.")
+            return
+        
+        if self.comendo:
+            print(f"{self.nome} está comendo, nao pode falar.")
+            return     
+           
+        self.falando = True
+        print(f"{self.nome} está falando sobre {assunto}.")
+        
+    def parar_falar(self):
+        if not self.falando:
+            print(f"{self.nome} não está falando")
+            return
+        
+        self.falando = False
+        print(f"{self.nome} parou de falar.")
+        
+    def get_ano_nascimento(self):
+        return self.ano_atual - self.idade
+    
+    def ano_nascimento(self):
+        print(f"{self.nome} nasceu em {self.get_ano_nascimento()}")
