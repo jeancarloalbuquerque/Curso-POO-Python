982a372e6f56194499d292df92d12dcfb92727ee
Jean Carlo Albuquerque <jeancarlo1584@gmail.com>
2023-02-07 01:49:16 -0400

    Aula 03

--- pessoa.py
+++ pessoa.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from random import randint
 
 
 class pessoa:
@@ -59,3 +60,8 @@ class pessoa:
     def por_ano_nascimento(cls, nome, ano_nascimento):
         idade = cls.ano_atual - ano_nascimento
         return cls(nome, idade)
+    
+    @staticmethod
+    def gerar_id():
+        print(randint(1000, 9999))
+        return 
