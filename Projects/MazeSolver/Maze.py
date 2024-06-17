5a6f385cb1c26e57dae478f1a43e6ea14b81e09f
Jean Carlo Albuquerque <jeancarlo1584@gmail.com>
2024-06-17 17:07:13 -0400

    update map method

--- Projects/MazeSolver/Maze.py
+++ Projects/MazeSolver/Maze.py
@@ -1,18 +1,20 @@
 import sys
 
-
 class Maze:
-    def parse(self, input):
-        return input
-    
+    def __init__(self) -> None:
+        self.direction = 'up'
+        self.origin = [0,0]
+        self.destination = [0,0]
+        pass
+
     def read(self):
         self.size = int(input())
-        self.lines = []
+        self.readLines = []
 
         for i in range(2 * self.size + 1):
             line = input()
             if line:
-                self.lines.append(line)
+                self.readLines.append(line)
             else:
                 break
 
@@ -21,9 +23,16 @@ class Maze:
     def map(self):
         self.map = []
 
-        for line in self.lines:
+        for line in self.readLines:
             self.map.append(list(line))
 
+        for l, line in enumerate(self.map):
+            if 'O' in line:
+                self.origin[0] = l
+                self.origin[1] = line.index('O')
+            if 'X' in line:
+                self.destination[0] = l
+                self.destination[1] = line.index('X')
         return self
     
     def render(self):
@@ -31,3 +40,6 @@ class Maze:
             for chunk in line:
                 print(chunk, end='')
             print('\r')
+
+    # def leftHand(self):
+        
--- Projects/MazeSolver/Solver.py
+++ Projects/MazeSolver/Solver.py
@@ -3,4 +3,7 @@ from Maze import Maze
 maze = Maze().read().map()
 
 maze.render()
+print("\nOrigin: ", maze.origin)
+print("Destination: ", maze.destination)
+# maze.leftHand()
 
